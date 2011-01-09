37641140691fd6c12ba14799c3f2fcf99b763ecc
wroclove <wroclove@f7e2f70a-a845-70aa-34e7-53a1c39fec84>
2011-01-09 13:33:29 +0000

    fixed path to smileys in bbcode

--- trunk/web/CMS/nbbc-wrapper.php
+++ trunk/web/CMS/nbbc-wrapper.php
@@ -5,7 +5,7 @@
 		{
 			require_once((dirname(__FILE__)) . '/nbbc-1.4.5/nbbc.php');
 			$setup = new BBCode;
-			$setup->SetSmileyURL(baseaddress() . 'smileys');
+			$setup->SetSmileyURL(baseaddress() . '/CMS/nbbc-1.4.5/smileys');
 //			$setup->SetEnableSmileys(false);
 			
 			return $setup->Parse($string);
